1d0084fd842aa720754646fec65c687332122d6b
gkelly <gkelly@groupon.com>
2015-12-07 11:06:15 +0000

    Project 1 - Hello World

---
+++ pr1/src/main/java/com/gkelly/spark/Main.java
@@ -0,0 +1,10 @@
+package com.gkelly.spark;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
+
