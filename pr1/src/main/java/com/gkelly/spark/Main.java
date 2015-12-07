565cfd01d7c91525ef8ad59e4c0a40cddc6eb9f1
gkelly <gkelly@groupon.com>
2015-12-07 11:26:33 +0000

    '/' redirects to the '/hello' endpoint

--- pr1/src/main/java/com/gkelly/spark/Main.java
+++ pr1/src/main/java/com/gkelly/spark/Main.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> {res.redirect("/hello", 301); return "";});
     }
 }
 
